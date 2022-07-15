e22a63c16318f22fed1e2a855e31518d44ee6daf
fcamussi <fcamussi@gmail.com>
2022-07-15 19:34:15 -0300

    practica6

---
+++ Practica6/app/src/main/java/com/example/practica6/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.practica6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOnClick(View view) {
+        Toast.makeText(this, "Hola! Soy Tux!", Toast.LENGTH_LONG).show();
+    }
+}
