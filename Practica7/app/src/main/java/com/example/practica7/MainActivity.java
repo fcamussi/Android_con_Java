7da5d07f68fe3e227229b3cef67deb4b98fddc68
fcamussi <fcamussi@gmail.com>
2022-07-16 13:57:13 -0300

    practica7

--- Practica7/app/src/main/java/com/example/practica7/MainActivity.java
+++ Practica7/app/src/main/java/com/example/practica7/MainActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etValor = findViewById(R.id.et_valor);
     }
 
     public void onClickSiguiente(View view) {
-        Intent siguiente = new Intent(this, SegundoActivity.class);
-        startActivity(siguiente);
+        Intent i = new Intent(this, SegundoActivity.class);
+        i.putExtra("valor", etValor.getText().toString());
+        startActivity(i);
     }
 
 }
--- Practica7/app/src/main/java/com/example/practica7/SegundoActivity.java
+++ Practica7/app/src/main/java/com/example/practica7/SegundoActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SegundoActivity extends AppCompatActivity {
+
+    private TextView tvValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+
+        tvValor = findViewById(R.id.tv_valor);
+        String valor = getIntent().getStringExtra("valor");
+        tvValor.setText(valor);
     }
 
     public void onClickAnterior(View view) {
