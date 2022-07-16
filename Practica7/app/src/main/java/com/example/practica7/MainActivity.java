9f39bc7cc32fa8ba82fc46dce5f5670ee338aba2
fcamussi <fcamussi@gmail.com>
2022-07-16 17:24:34 -0300

    practica7 - finalizada

--- Practica7/app/src/main/java/com/example/practica7/MainActivity.java
+++ Practica7/app/src/main/java/com/example/practica7/MainActivity.java
@@ -2,27 +2,38 @@ package com.example.practica7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etValor;
+    private EditText etURL;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etValor = findViewById(R.id.et_valor);
+        etURL = findViewById(R.id.et_url);
+        preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        etURL.setText(preferences.getString("url", ""));
     }
 
-    public void onClickSiguiente(View view) {
+    public void onClickIraURL(View view) {
         Intent i = new Intent(this, SegundoActivity.class);
-        i.putExtra("valor", etValor.getText().toString());
+        i.putExtra("url", etURL.getText().toString());
         startActivity(i);
+    }
+
+    public void onClickGuardar(View view) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("url", etURL.getText().toString());
+        editor.commit();
     }
 
 }
--- Practica7/app/src/main/java/com/example/practica7/SegundoActivity.java
+++ Practica7/app/src/main/java/com/example/practica7/SegundoActivity.java
@@ -2,27 +2,30 @@ package com.example.practica7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SegundoActivity extends AppCompatActivity {
 
-    private TextView tvValor;
+    private WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
 
-        tvValor = findViewById(R.id.tv_valor);
-        String valor = getIntent().getStringExtra("valor");
-        tvValor.setText(valor);
+        wv = findViewById(R.id.webView);
+        String url = getIntent().getStringExtra("url");
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl(url);
     }
 
     public void onClickAnterior(View view) {
-        Intent anterior = new Intent(this, MainActivity.class);
-        startActivity(anterior);
+        //Intent i = new Intent(this, MainActivity.class);
+        //startActivity(i);
+        finish();
     }
 }
