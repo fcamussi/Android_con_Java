6541cb3bdc9d9f2f5332a1342ce7473a97006ca8
fcamussi <fcamussi@gmail.com>
2022-07-22 19:25:04 -0300

    Practica 5 - adapter

--- Practica5/app/src/main/java/com/example/practica5/MainActivity.java
+++ Practica5/app/src/main/java/com/example/practica5/MainActivity.java
@@ -6,14 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
+    private EditText et;
     private ListView lv;
-    private final String [] colores = {"Rojo", "Verde", "Azul"};
+    private ArrayList<String> colores;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = findViewById(R.id.tv);
+        et = findViewById(R.id.et);
         lv = findViewById(R.id.lv);
+        colores = new ArrayList<>();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        adapter = new ArrayAdapter<>(this,
                 R.layout.custom_list_item, colores);
         lv.setAdapter(adapter);
+        adapter.add("Rojo");
+        adapter.add("Verde");
+        adapter.add("Azul");
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 tv.setText(lv.getItemAtPosition(i).toString());
             }
         });
+    }
+
+    public void onClickAgregar(View view) {
+        adapter.add(et.getText().toString());
     }
 
 }
